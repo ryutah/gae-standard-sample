70f8218482a7e43c216cc910e0dddb4751b54871
ryutah <ryuta0221@gmail.com>
2018-11-26 11:25:20 +0900

    feat(go111): add go111 helloworld application

---
+++ go111/helloworld/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/golang/glog"
+)
+
+func main() {
+	flag.Set("stderrthreshold", "INFO")
+	flag.Parse()
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		glog.Infof("hello!!")
+		glog.Infof("context: %#v", r.Context())
+		for k, v := range r.Header {
+			glog.Infof("%s: %v", k, v)
+		}
+		fmt.Fprintln(w, "Hello World!!")
+	})
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
+}
